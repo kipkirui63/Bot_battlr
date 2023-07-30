75c0e69f2d297fd46f153d0283054fbc11003d56
kipkirui63 <enocksang8356@gmail.com>
2023-07-30 15:39:48 +0300

    asiigned the diiferent specs for bots to displayed when sort by the spec chosen/sortby

---
+++ src/components/BotSpecs.js
@@ -0,0 +1,22 @@
+// components/BotSpecs.js
+import React from 'react';
+
+const BotSpecs = ({ bot, goBackToListView, enlistBot }) => {
+  return (
+    <div className="bot-specs">
+      <h2>Bot Specs</h2>
+      <div className="bot-card">
+        <img src={bot.avatar_url} alt={bot.name} />
+        <h3>{bot.name}</h3>
+        <p>Health: {bot.health}</p>
+        <p>Damage: {bot.damage}</p>
+        <p>Armor: {bot.armor}</p>
+        <p>Class: {bot.bot_class}</p>
+        <p>Catchphrase: {bot.catchphrase}</p>
+        
+      </div>
+    </div>
+  );
+};
+
+export default BotSpecs;
