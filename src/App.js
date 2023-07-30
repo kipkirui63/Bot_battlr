b1adc980c0f9db2e08ddcc57cd1bb4118a9bef86
kipkirui63 <enocksang8356@gmail.com>
2023-07-30 15:44:21 +0300

    imported the exported files in ./components

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
+import BotFilterSort from './components/BotFilterSort';
+import BotSpecs from './components/BotSpecs';
