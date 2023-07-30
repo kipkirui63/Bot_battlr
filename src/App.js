d0e9ad5324860b5ccf0971f0537c417157bbccb6
kipkirui63 <enocksang8356@gmail.com>
2023-07-30 15:47:47 +0300

    imported the exported files in ./components

--- src/App.js
+++ src/App.js
@@ -4,3 +4,10 @@ import BotCollection from './components/BotCollection';
 import YourBotArmy from './components/YourBotArmy';
 import BotFilterSort from './components/BotFilterSort';
 import BotSpecs from './components/BotSpecs';
+
+const App = () => {
+  const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
+  const [filterBy, setFilterBy] = useState('');
+  const [sortBy, setSortBy] = useState('name');
+  const [selectedBot, setSelectedBot] = useState(null);
